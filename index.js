0732c66cbfd7aa31af35c422e12961a7808371bd
Ajowi254 <ajowi.bery@gmail.com>
2023-06-15 13:36:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+
+  function receivesAFunction(){
+
+  }
+  function receivesAFunction(){
+  
+  }
+  
+  function receivesAFunction(spy){
+  spy()
+  }
+  function returnsANamedFunction() {
+      return function namedFunction() {
+        // function body
+      };
+    }
+  
+    function returnsAnAnonymousFunction() {
+      return function() {
+        // function body
+      };
+    }
